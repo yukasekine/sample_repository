0d647f9b1702015059eb5e681265d9474fb202bb
yukasekine <y.sekine.cingroup2012@gmail.com>
2024-05-13 12:21:11 +0900

    phplesson

---
+++ phplesson/01.値と型と変数/lesson3.php
@@ -0,0 +1,29 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+
+
+$profile = array(
+  'name'   => '山田',
+  'age'    => 20,
+  'gender' => '女性',
+);
+echo $profile['name'];
+echo '<br>';
+
+$age = 20;
+$gender = '女性';
+
+echo $age.'歳・'.$gender;
+echo '<br>';
+
+
+
+

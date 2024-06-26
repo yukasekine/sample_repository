b56d45582aa5b72feae7a6e0b4ca556e617d9826
yukasekine <y.sekine.cingroup2012@gmail.com>
2024-06-26 12:04:29 +0900

    Remove all files from main branch

--- phplesson/05.関数/lesson1.php
+++ phplesson/05.関数/lesson1.php
@@ -2,11 +2,12 @@
 // 引数として数値を渡すと3倍にして返す関数を作ってください。
 function convertThreeTimes($num)
 {
-    echo $num * 3;
+    $result = $num * 3;
+    return $result;
 }
-convertThreeTimes($num);
 
 //テスト検証
-$num_test = 5;
-echo convertThreeTimes($num_test);
+$test_number = 5;
+$num_test = convertThreeTimes($test_number);
+echo $num_test;
 // 15
